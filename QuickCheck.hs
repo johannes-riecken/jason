e45de98ceaa5605b03ae94ffcac565f8ccfae521
Johannes Riecken <johannes.riecken@gmail.com>
2021-12-09 22:54:12 +0100

    Add property-based test

---
+++ QuickCheck.hs
@@ -0,0 +1,19 @@
+import Test.QuickCheck
+import Text.Printf
+import System.IO.Unsafe
+import System.Process
+
+jason :: String -> String
+jason str = unsafePerformIO $ readProcess "./jason" [] str
+
+j :: String -> String
+j str = unsafePerformIO $ readProcess "jconsole" [] str
+
+newtype JInput = JInput String deriving (Show)
+
+instance Arbitrary JInput where
+    arbitrary = JInput . printf "i. %d" <$> (arbitrary :: Gen Int)
+
+
+main :: IO ()
+main = quickCheck $ \(JInput x) -> jason x === j x
